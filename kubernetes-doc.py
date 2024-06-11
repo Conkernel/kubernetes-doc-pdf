3c3deba69e9b444f1fb759ccdeea52ab66490de4
Conkernel <emaildejuanca@gmail.com>
2024-06-11 22:26:29 +0200

    Updated to use 2 langs
    Needs a lot of improvement and leaning

--- kubernetes-doc.py
+++ kubernetes-doc.py
@@ -1,63 +1,97 @@
 import requests_html as rh
 import os
-# import pypandoc
 import subprocess
+import requests
+import json
+from pathlib import Path
 
-def generate_directory_pdf(url, name, s=None):
+# to change language, set the content of "lang" to iso code. Also, please check that it already exist on k8s web
+lang = "es"
+
+def generate_directory_pdf(url1, name, s=None):
+    # some needed variables...
+    mydir = Path(f"tmp/links_{name}")
+    mydir.mkdir(parents=True, exist_ok=True)
+    final_links_to_download = f"tmp/links_{name}/links_to_download.json"
+    url2 = f"https://kubernetes.io/{lang}/docs/{name}"
+
     s = rh.HTMLSession() if not s else s
-    r1 = s.get(url)
-
+    r1 = s.get(url1)
+    r2 = s.get(url2)
     html = ""
-    anchors = r1.html.find('.td-sidebar-link')
-    links = [a.absolute_links.pop() for a in anchors if a.element.tag == 'a']
-    # dedup
-    unique_links = []
-    for i in links:
-        if i not in unique_links:
-            unique_links.append(i)
-    links = unique_links
-    links = filter(lambda href: href.startswith(url), links) # filter out links
+    anchors1 = r1.html.find('.td-sidebar-link')
+    anchors2 = r2.html.find('.td-sidebar-link')
+    links_en = [a.absolute_links.pop() for a in anchors1 if a.element.tag == 'a']
+    links_es = [a.absolute_links.pop() for a in anchors2 if a.element.tag == 'a']
 
-    print("downloading...")
+    links_en_uniq_a_comprobar = []
+    for i in links_en:
+        if i not in links_en_uniq_a_comprobar:
+            links_en_uniq_a_comprobar.append(i)
+
+    links_solo_es_uniq = []
+    for i in links_es:
+        if i not in links_solo_es_uniq:
+            links_solo_es_uniq.append(i)
+
+    links_es_uniq_a_comprobar = []
+    links_es_uniq_a_comprobar = [link.replace("kubernetes.io/docs", "kubernetes.io/{lang}/docs") for link in links_en_uniq_a_comprobar]
+
+    def check_url(tocheck):
+        try:
+            response = requests.get(tocheck, timeout=5)
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+        except requests.RequestException:
+            return False
+
+    checked_links_mixed = []
+    for english, spanish in zip(links_en_uniq_a_comprobar, links_es_uniq_a_comprobar):
+        if check_url(spanish):
+            checked_links_mixed.append(spanish)
+        else:
+            checked_links_mixed.append(english)
+
+    mixed_links_to_uniq = checked_links_mixed + links_solo_es_uniq
+    filtered_mixed_links_for_lambda = []
+    for i in mixed_links_to_uniq:
+        if i not in filtered_mixed_links_for_lambda:
+            filtered_mixed_links_for_lambda.append(i)
+
+
+    links_post_lambda = filter(lambda href: href.startswith(url1) or href.startswith(url2), filtered_mixed_links_for_lambda)
+    links_post_lambda_list = list(links_post_lambda)
+
+
+    with open(final_links_to_download, 'w') as output_file:
+                json.dump(links_post_lambda_list, output_file, indent=4)
+
+    print("Downloading content from links...")
     cwd = os.getcwd()
-    for l1 in links:
+    for l1 in links_post_lambda_list:
         r2 = s.get(l1)
-        # r2.html.render()
         div = r2.html.find('.td-content', first=True, clean=True)
         if div:
-            # try:
-                # if name in ["Setup", "Tutorials", "Reference"]:  # will give duplicate id error, go through pages one by one to skip error page
-                    # print("testing " + l1, end='')
-                    # output = pypandoc.convert_text(div.html, "pdf", format="html", outputfile="/tmp/kubernetes_pdf_doc_tmp.pdf".format(name), extra_args=['--pdf-engine=weasyprint'])
-                    # print(" works")
-            # except Exception as e:
-            #     print(" failed!")
-            #     print(e)
-            # else:
             html += div.html
         with open("{}/{}.html".format(cwd, name), "wt") as f:
             f.write(html)
 
-    print("generating pdf...")
+    print("generating pdf in " + name )
     subprocess.run(["{}/weasy_print.sh".format(cwd), name])
-    # try:
-    #     output = pypandoc.convert_text(html, "pdf", format="html", outputfile="./{}.pdf".format(name), extra_args=['--pdf-engine=weasyprint', '--css=codeblock_wrap.css'])
-    # except Exception as e:
-    #     print(e)
+
 
 if __name__ == '__main__':
     s = rh.HTMLSession()
     directories = [\
-                   "Setup",
-                   "Concepts",
-                   "Tasks",
-                   "Tutorials",
-                   "Reference",
-                   # "Getting-started-guides",  # same as setup
-                   # "Admin",  # this direct to concepts
-                   # "Imported",  # deprecated
+                   "setup",
+                   "concepts",
+                   "tasks",
+                   "tutorials",
+                   "reference",
                    ]
     directories_pairs = [("https://kubernetes.io/docs/{}/".format(n.lower()), n) for n in directories]
-    for url, name in directories_pairs:
-        print(name)
-        generate_directory_pdf(url, name)
+    for url1, name in directories_pairs:
+        print("Working with the content in url : " + url1)
+        generate_directory_pdf(url1, name)
